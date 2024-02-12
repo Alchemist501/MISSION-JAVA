3e3ed31460756d816601682377a9f3b7b6601ea9
Siya <ayispauljc@gmail.com>
2024-02-12 19:52:08 +0530

    Updated

---
+++ Students.java
@@ -0,0 +1,8 @@
+/*1)Read 3 marks of a student from an input file with following format 
+=================================
+Roll.No Name Mark1 Mark2
+=================================
+111     Anu    50    50
+
+Display the above details in a GUI when clicking on "Import data" button. Use required Textbox and labels When clicking on "Calculate" button, find total marks and display it in another text box*/
+
